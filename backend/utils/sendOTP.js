e6617586c45bb5af70c7776c7101ba862a1802d9
Virendra Yadav <viru.4231@gmail.com>
2026-06-17 11:46:46 +0530

    registering problem solving

--- backend/utils/sendOTP.js
+++ backend/utils/sendOTP.js
@@ -3,21 +3,12 @@ const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendOTP = async (email, otp) => {
-    try {
-        const info = await transporter.sendMail({
-            from: process.env.EMAIL_USER,
-            to: email,
-            subject: "Email Verification OTP",
-            text: `Your OTP is ${otp}. It will expire in 5 minutes.`,
-        });
-
-        console.log("OTP sent successfully");
-        console.log(info);
-
-    } catch (err) {
-        console.error("Email Error:", err);
-        throw err;
-    }
+    await resend.emails.send({
+        from: "onboarding@resend.dev",
+        to: email,
+        subject: "Email Verification OTP",
+        text: `Your OTP is ${otp}. It will expire in 5 minutes.`,
+    });
 };
 
 module.exports = sendOTP;
