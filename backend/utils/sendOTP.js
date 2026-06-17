563279e8ef3e29875c4bee880ceecd758b7aa0aa
Virendra Yadav <viru.4231@gmail.com>
2026-06-17 12:37:56 +0530

    debuggin resend

--- backend/utils/sendOTP.js
+++ backend/utils/sendOTP.js
@@ -1,8 +1,12 @@
 const { Resend } = require("resend");
 
+console.log("USING RESEND VERSION");
+
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendOTP = async (email, otp) => {
+    console.log("Sending OTP via Resend");
+
     await resend.emails.send({
         from: "onboarding@resend.dev",
         to: email,
